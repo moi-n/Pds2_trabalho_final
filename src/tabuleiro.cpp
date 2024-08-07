bf5763946c644a8cb5773b7e04da4bb8beea35fb
Vitor Bert <117232893+VithMB@users.noreply.github.com>
2024-08-06 22:56:04 -0300

    Update tabuleiro.cpp
    indices na impressao do tabuleiro na *void Tabuleiro::imprimeTabuleiro()*.
    
    Tirei as definiçoes das funçoes virtuais pura.
    
    Como os nomes agora sao *num_linhas* e *num_colunas*, os nomes dos argumentos nas funçoes mudaram junto *_num_linhas* e *_num_colunas*

--- src/tabuleiro.cpp
+++ src/tabuleiro.cpp
@@ -3,20 +3,22 @@
 #include "casa.hpp"
 #include "tabuleiro.hpp"
 
-Tabuleiro::Tabuleiro(int _tam_linha, int _tam_coluna) : tam_linha(_tam_linha), tam_coluna(_tam_coluna) {
+Tabuleiro::Tabuleiro(int _num_linhas, int _num_colunas) : num_linhas(_num_linhas), num_colunas(_num_colunas) {
 
     // inicia com o primeiro jogador (1)
     this->jogador = 1; 
     // inicia com 0, termina com vencedor sendo (1) ou (2)
     this->vencedor = 0;
-    m_tabuleiro = std::vector<std::vector<Casa>>(this->tam_linha, std::vector<Casa>(this->tam_coluna, Casa()));
+    matriz_tabuleiro = std::vector<std::vector<Casa>>(this->num_linhas, std::vector<Casa>(this->num_colunas, Casa()));
 }
 
 Tabuleiro::~Tabuleiro(){}
 
 std::vector<std::vector<Casa>>& Tabuleiro::getTabuleiro(){
-    return m_tabuleiro;
+    return matriz_tabuleiro;
 }
+
+
 
 void Tabuleiro::setJogador(int _jogador) {
     // alterna entre o jogador (1) e jogador (2)
@@ -39,26 +41,35 @@ int Tabuleiro::getVencedor() {
 void Tabuleiro::imprimeTabuleiro() {
 
     std::cout << std::endl;
-    for (int i = 0; i < this->tam_linha; i++) {
-        std::cout << "| ";
-        for (int j = 0; j < this->tam_coluna; j++) {
-            std::cout << m_tabuleiro[i][j].getConteudo() << " | ";
+    for (int i = 0; i < this->num_linhas; i++) {
+        std::cout << i+1 << "  _" << "| ";
+
+        for (int j = 0; j < this->num_colunas; j++) {
+            std::cout << matriz_tabuleiro[i][j].getConteudo() << " | ";
         }
         std::cout << std::endl;
     }
+    std::cout << "   ";
+    for (int j = 0; j < this->num_colunas; j++) {
+        std::cout << " / " << j+1;
+    }
+    std::cout << " / ";
     std::cout << std::endl;
+    
+        
 }
+
 
 void Tabuleiro::trocaConteudoCasa(int linha, int coluna, int jogador) {
 
     if (jogador == 1){
-        m_tabuleiro[linha-1][coluna-1].setConteudo("X");
-        m_tabuleiro[linha-1][coluna-1].setEstado(1); // define a casa como 1 = preenchida pelo jogador1, 0 = vazia
+        matriz_tabuleiro[linha-1][coluna-1].setConteudo("X");
+        matriz_tabuleiro[linha-1][coluna-1].setEstado(1); // define a casa como 1 = preenchida pelo jogador1, 0 = vazia
     }
 
     else if (jogador == 2) {
-        m_tabuleiro[linha-1][coluna-1].setConteudo("O");
-        m_tabuleiro[linha-1][coluna-1].setEstado(2); // define a casa como 2 = preenchida pelo jogador2, 0 = vazia
+        matriz_tabuleiro[linha-1][coluna-1].setConteudo("O");
+        matriz_tabuleiro[linha-1][coluna-1].setEstado(2); // define a casa como 2 = preenchida pelo jogador2, 0 = vazia
     }
 
     else{
@@ -70,12 +81,13 @@ void Tabuleiro::pegaJogada(int jogador) {
 
     int linha, coluna;
     
-    std::cout << "Jogador " << jogador << ", digite sua jogada, linha e coluna: ";
+    std::cout << "Jogador " << jogador << ", digite sua jogada, linha [SPACE] coluna: ";
     while(1) {
 
         std::cin >> linha >> coluna;
 
-        if (linha <= 0 || coluna <= 0 ||(linha) > tam_linha || (coluna) > tam_coluna || m_tabuleiro[linha-1][coluna-1].getEstado() != 0) {
+        if (linha <= 0 || coluna <= 0 || (linha) > num_linhas || (coluna) > num_colunas ||
+         matriz_tabuleiro[linha-1][coluna-1].getEstado() != 0) {
             
             std::cout << "Jogada inválida, digite novamente: ";
             continue;
@@ -86,24 +98,16 @@ void Tabuleiro::pegaJogada(int jogador) {
             break;
         }
     }
+
 }
 
 int Tabuleiro::checaOcupacaoTotal() { // retorna 1 se tiver totalmente ocupado 
-    for (int i = 0; i < tam_linha; ++i) {
-        for (int j = 0; j < tam_coluna; ++j) {
-            if (m_tabuleiro[i][j].getEstado() == 0) {
+    for (int i = 0; i < num_linhas; ++i) {
+        for (int j = 0; j < num_colunas; ++j) {
+            if (matriz_tabuleiro[i][j].getEstado() == 0) {
                 return 0; 
             }
         }
     }
     return 1; 
 }
-// classes a serem redefinidas nas classes filhas de acordo com cada regra de jogo
-
-int Tabuleiro::checaLinha(int linha) {}
-
-int Tabuleiro::checaColuna(int coluna) {}
-
-int Tabuleiro::checaDiagonal() {}
-
-int Tabuleiro::checaVitoria() {}
