f0bd9f8c0acbb1ee22e6489da58ad62b948a3d4e
Chris <119696441+Christian-ozy@users.noreply.github.com>
2024-08-16 13:01:51 -0300

    Add files via upload

--- include/gomoku.hpp
+++ include/gomoku.hpp
@@ -4,9 +4,9 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include "tabuleiro.hpp"
+#include "tabuleirofileira.hpp"
 
-class Gomoku : public Tabuleiro{
+class Gomoku : public TabuleiroFileira{
 
     public:
         Gomoku(int linha_, int coluna_);
@@ -16,10 +16,8 @@ class Gomoku : public Tabuleiro{
         int checaLinha(int linha) override; // retorna 1 se jogador 1 ganho, 2 same, 0 se nenhum
         int checaColuna(int coluna) override;// retorna 1 se jogador 1 ganho, 2 same, 0 se nenhum
         int checaDiagonal() override;// retorna 1 se jogador 1 ganho, 2 same, 0 se nenhum
+        
         int checaVitoria() override;
-
-    private: 
-
 };
 
 #endif
