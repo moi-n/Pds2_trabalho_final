a73a120b271f54c51f1f90eb30f1744fa1b0281c
Alexânia <alexaniamariaufmg@gmail.com>
2024-08-18 19:18:38 -0300

    Update teste_jogador.cpp

--- tests/teste_jogador.cpp
+++ tests/teste_jogador.cpp
@@ -3,7 +3,7 @@
 #include "doctest.h"
 #include "jogador.hpp"
 
-TEST_CASE("Testando as excessões de jogador") {
+TEST_CASE("Testando as exceções de jogador") {
     CHECK_THROWS_AS(Jogador::setJogador("", ""),std::invalid_argument);
     CHECK_THROWS_AS(Jogador teste("testeunitario"), std::invalid_argument);
     
