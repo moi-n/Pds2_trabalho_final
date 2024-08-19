5ebd6056b895cbf414e0b832ec746d909b84305c
LucasMeneguett <lucasmmt2003@gmail.com>
2024-08-19 03:50:47 -0300

    Add files via upload

---
+++ include/tabuleiroDamas.hpp
@@ -0,0 +1,30 @@
+#ifndef TABULEIRO_DAMAS_H
+#define TABULEIRO_DAMAS_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "casa.hpp"
+#include "tabuleiro.hpp"
+
+class TabuleiroDamas : public Tabuleiro {
+
+  public:
+    //inicializadores e destrutores
+    TabuleiroDamas();
+    TabuleiroDamas(int linhas, int colunas);
+    virtual ~TabuleiroDamas();
+
+    //getters e setters
+
+    //métodos
+    int verificaCaptura(int linha_peca, int coluna_peca);
+    void verificaJogada(int linha_peca, int coluna_peca, int linha_destino, int coluna_destino, int capturaObrigatoria);
+    void pegaJogada() override;
+    void trocaConteudoCasa(int linha, int coluna, std::string conteudo) override;
+    void limpaConteudoCasa(int linha, int coluna);
+    int checaVitoria() override;
+  
+}
+
+#endif
